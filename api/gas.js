7223e50300a8c8b4dbbabb52d9d040bead7ae236
rk2348 <ryoma.k0718@icloud.com>
2026-07-29 19:37:40 +0900

    Update gas.js

--- api/gas.js
+++ api/gas.js
@@ -7,7 +7,7 @@
 
 module.exports = async function handler(req, res) {
   // ここにご自身のGASウェブアプリのURLを入れてください
-  const GAS_URL = "https://script.google.com/macros/s/AKfycbzF_yc43D4sUpN3pTbhG-u3Gkb9porCVuoqH7rhWJk5ObYu0glbZuP2tzB_7i8vTZK2/exec";
+  const GAS_URL = "https://script.google.com/macros/s/AKfycbwUcFJh0RhHx4wooQ1csdmrZ6UWWLswlx3YZoenKjcjUI8LvNJ5ezO_PYMp6ZYXJ0w/exec";
 
   try {
     const targetUrl = new URL(GAS_URL);
